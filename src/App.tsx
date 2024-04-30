c35ace42d4ec4fa8f3bf3f51e1b6150059e6ac44
tchkoidze <ilia.chkoidze12@gmail.com>
2024-04-30 11:02:58 +0400

    add suspance at app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+//import Home from "./views/Home/Home";
+import { Suspense, lazy } from "react";
+
+const Home = lazy(() => import("@src/views/Home/Home"));
 
 function App() {
   return (
-    <>
+    <Suspense>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Suspense>
   );
 }
 
