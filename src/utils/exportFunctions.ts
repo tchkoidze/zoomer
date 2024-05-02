613ff169ddfb292ad24a6b0e025acafc3aedaa15
tchkoidze <ilia.chkoidze12@gmail.com>
2024-05-02 21:12:35 +0400

    add products page

---
+++ src/utils/exportFunctions.ts
@@ -0,0 +1,21 @@
+import { CartITem } from "@src/@types/types";
+
+export function getFullPrice(cartItems: CartITem[]) {
+  let fullPrice: number = 0;
+  for (let item of cartItems) {
+    for (let i = 0; i < item.count; i++) {
+      fullPrice += item.cartProduct.salePrice || item.cartProduct.price;
+    }
+  }
+  return fullPrice;
+}
+
+export const modifyDate = (created_at: string) => {
+  const createdDate: Date = new Date(created_at);
+  const formattedDate = createdDate.toISOString().split("T")[0]; // Extracting date part
+  const formattedTime = createdDate.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  }); // Formatting time
+  return `${formattedDate} ${formattedTime}`;
+};
---
+++ src/views/Products/ProductsList/ProductsList.tsx
@@ -0,0 +1,24 @@
+import { Product } from '@src/@types/types'
+import ProductCard from '@src/components/ProductCard/ProductCard'
+import PaginationButtons from '@src/features/Pagination/PaginationButtons';
+
+interface ProductsListProps {
+    products: Product[];
+    totalProducts: number;
+}
+
+export default function ProductsList({products, totalProducts} : ProductsListProps) {
+    
+    const productCards = products.map((prod:Product)=> {
+        return <ProductCard key={prod.id} product={prod}/>
+    })
+
+    return (
+        <div className='w-full'>
+            <div className=' w-full lg:pl-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 gap-6 ml-auto'>{productCards}</div>
+            <div className='flex justify-center mt-14'>
+                  <PaginationButtons totalProducts={totalProducts as number} />
+            </div>
+        </div>
+    )
+}
